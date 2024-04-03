d3cfab52cc5751884c5b88f8a45adfe7bbeb9eca
Falguni Suthar <falguni@green-apex.com>
2024-04-03 11:52:57 +0530

    Setup with mongodb and error handling using exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,17 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { configs } from './config/config';
 
 @Module({
-  imports: [AuthModule, UsersModule, ProductsModule, OrdersModule],
+  imports: [
+    MongooseModule.forRoot(configs.mongoUrl),
+    AuthModule,
+    UsersModule,
+    ProductsModule,
+    OrdersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
